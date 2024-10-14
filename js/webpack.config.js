e2346be776c36671551e75d9d1e0741d22aa8378
Melso <melsopv@gmail.com>
2024-10-14 15:28:53 +0200

    small changes

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,22 +1,10 @@
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-  context: path.resolve(__dirname), // Add this line to set context explicitly
-  entry: './js/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  mode: 'development', // Ensure this is set properly
-  resolve: {
-    extensions: ['.js'], // Allow importing files without specifying extensions
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.APIKEY': JSON.stringify(process.env.APIKEY),
-      'process.env.SERVICEID': JSON.stringify(process.env.SERVICEID),
-      'process.env.TEMPLATEID': JSON.stringify(process.env.TEMPLATEID),
-    }),
-  ],
+    entry: './js/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'production',
 };
