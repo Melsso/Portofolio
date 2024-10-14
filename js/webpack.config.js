8720d6d0f910cae61b4f167625ef730fcb1145ef
Melso <melsopv@gmail.com>
2024-10-14 15:45:48 +0200

    it wasnt the last change!

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
 module.exports = {
     entry: './js/main.js',
     output: {
@@ -9,4 +8,11 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
     },
     mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.SERVICEID': JSON.stringify(process.env.SERVICEID),
+            'process.env.TEMPLATEID': JSON.stringify(process.env.TEMPLATEID),
+            'process.env.APIKEY': JSON.stringify(process.env.APIKEY),
+        }),
+    ],
 };
