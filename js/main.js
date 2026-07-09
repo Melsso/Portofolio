00901c12d4525608a0f19bbe86d59d8953b28f8b
Mallem Sofiane <melsopv@gmail.com>
2026-07-09 20:29:54 +0200

    cleaning up display bug, added new project to list, updated resume

--- js/main.js
+++ js/main.js
@@ -1,94 +1,104 @@
 jQuery(document).ready(function($) {
-  'use strict';
-  var owl = $("#owl-testimonials");
+    'use strict';
+    var owl = $("#owl-testimonials");
     owl.owlCarousel({ 
-      pagination : true,
-      paginationNumbers: false,
-      autoPlay: 6000,
-      items : 1,
-      itemsDesktop : [1000,1],
-      itemsDesktopSmall : [900,1],
-      itemsTablet: [600,1],
-      itemsMobile : false    
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000,
+        items : 1,
+        itemsDesktop : [1000,1],
+        itemsDesktopSmall : [900,1],
+        itemsTablet: [600,1],
+        itemsMobile : false    
     });
     var top_header = $('.parallax-content');
     top_header.css({'background-position':'center center'});
+    
     $(window).scroll(function () {
-      var st = $(this).scrollTop();
-      top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+        var st = $(this).scrollTop();
+        top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
     });
+    
     $('.counter').each(function() {
         var $this = $(this),
         countTo = $this.attr('data-count');  
         $({ countNum: $this.text()}).animate({
-          countNum: countTo
+        countNum: countTo
         },
 
         {
-          duration: 8000,
-          easing:'linear',
-          step: function() {
+        duration: 8000,
+        easing:'linear',
+        step: function() {
             $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
+        },
+        complete: function() {
             $this.text(this.countNum);
-          }
+        }
         });  
-      });
-      $(".pop-button").click(function () {
-          $(".pop").fadeIn(300);
-      });
-      $(".pop > span").click(function () {
-          $(".pop").fadeOut(300);
-      });
-      $(window).on("scroll", function() {
+    });
+    
+    $(".pop-button").click(function () {
+        $(".pop").fadeIn(300);
+    });
+    $(".pop > span").click(function () {
+        $(".pop").fadeOut(300);
+    });
+    $(window).on("scroll", function() {
         if($(window).scrollTop() > 100) {
-          $(".header").addClass("active");
-          } else {
+        $(".header").addClass("active");
+        } else {
             $(".header").removeClass("active");
-          }
-      });
-      $('.projects-holder').mixitup({
+        }
+    });
+    $('.project-filter .filter').click(function() {
+        var filterValue = $(this).data('filter');
+        $('#cont').fadeIn(2000);
+        $('.projects-holder').mixitup('filter', '.' + filterValue);
+    });
+    
+    $('.projects-holder').mixitup({
         effects: ['fade','grayscale'],
-          easing: 'snap',
-          transitionSpeed: 400,
-  });
+        easing: 'snap',
+        transitionSpeed: 400,
+    });
+    $('.project-filter .filter[data-filter="hang"]').trigger('click');
 });
 
 
 const form = document.getElementById('contact');
 
 form.addEventListener('submit', function (event) {
-  event.preventDefault();
-  
-  const service_id = process.env.SERVICEID;
-  const template_id = process.env.TEMPLATEID;
-  const api_key = process.env.APIKEY;
+    event.preventDefault();
+
+    const service_id = process.env.SERVICEID;
+    const template_id = process.env.TEMPLATEID;
+    const api_key = process.env.APIKEY;
 
 
-  emailjs.init(api_key);
+    emailjs.init(api_key);
 
-  const t_name = document.getElementById('name').value;
-  const msg = document.getElementById('message').value;
+    const t_name = document.getElementById('name').value;
+    const msg = document.getElementById('message').value;
 
-  const tempParam = {
-    to_name: t_name,
-    message: msg,
-  };
+    const tempParam = {
+        to_name: t_name,
+        message: msg,
+    };
 
-  emailjs.send(service_id, template_id, tempParam)
+    emailjs.send(service_id, template_id, tempParam)
     .then(function (response) {
-      console.log('Success', response.status, response.text);
+        console.log('Success', response.status, response.text);
     }, function (error) {
-      console.log('error: ', error);
+        console.log('error: ', error);
     });
 });
 
 jQuery(document).ready(function($) {
-  'use strict';
-  $('.project-filter .filter').click(function() {
-      var filterValue = $(this).data('filter');
-      $('#cont').fadeIn(2000);
-      $('.projects-holder').mixitup('filter', '.' + filterValue);
-  });
+    'use strict';
+    $('.project-filter .filter').click(function() {
+        var filterValue = $(this).data('filter');
+        $('#cont').fadeIn(2000);
+        $('.projects-holder').mixitup('filter', '.' + filterValue);
+    });
 });
